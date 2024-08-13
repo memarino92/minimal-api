1093c1a08ee1bc4617721b0a092b11489aa7ef20
Michael <memarino92@gmail.com>
2024-08-12 20:23:42 -0400

    added hello name route

--- Program.cs
+++ Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/hello/{name}", (string name) => $"Hello {name}!");
 
 app.Run();
